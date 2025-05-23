5ab029df375eda3bfa2fe1c4d69834134ba531ea
kabilan-7-v <kabilanvelmani@gmail.com>
2025-05-23 21:25:47 +0530

    changes

--- DDA-Web/src/Pages/Servicespage/Relatedservices.jsx
+++ DDA-Web/src/Pages/Servicespage/Relatedservices.jsx
@@ -1,105 +1,51 @@
-import React from 'react'
-import './Relatedservices.css'
+import React from 'react';
+import './Relatedservices.css';
 import arrow from '../../assets/image/arrow-right.png'; 
 
-
-function Relatedservices() {
+function Relatedservices({ services }) {
   return (
-    <>
     <div className='relate'>
-        <div className='related'>
-                <div className='related-head'>
-            <div className='related-headcontent'>
-              <p className="related-p1">Our services</p>
-              <h1 className="related-h1">
-                Explore our related <br/> services
-              </h1>
-            </div>
-            <div className="related-b1">
-          <a href='/services'>   <button>
-                View All related services <img src={arrow} alt="Description" />
-        
-              </button></a> 
-            </div>
+      <div className='related'>
+        <div className='related-head'>
+          <div className='related-headcontent'>
+            <p className="related-p1">Our services</p>
+            <h1 className="related-h1">
+              Explore our related <br /> services
+            </h1>
           </div>
-          <div className='related-card'>
-           <a href="/webdevelopment">
-           <div className='rcard1'>
-                <div className='rcard1-top'>
-                <h2>Web <br />Development</h2>
-                <div class="rcard1-btn">
-                      <button><img src={arrow} alt="Description" /></button>
-                    </div>
-        
-                </div>
-                <hr />
-        
-                <div>
-                    <p>
-                      Custom, responsive websites that drive performance and visibility.
-                    </p>
-                  </div>
-                  <div class="rcard1-image">
-                  <img src="https://demo.awaikenthemes.com/weebix/wp-content/uploads/2024/04/service-img-1.jpg"/>
-                </div>
-        
-            </div></a>
-
-         <a href='/app-price'>   <div className='rcard1'>
-                <div className='rcard1-top'>
-                <h2>App <br /> Development</h2>
-                <div class="rcard1-btn">
-                      <button><img src={arrow} alt="Description" /></button>
-                    </div>
-        
-                </div>
-                <hr />
-        
-                <div>
-                    <p>
-                       Smart, user-friendly mobile applications for Android and iOS platforms.
-
-                    </p>
-                  </div>
-                  <div class="rcard1-image">
-                  <img src="https://demo.awaikenthemes.com/weebix/wp-content/uploads/2024/04/service-img-1.jpg" />
-                </div>
-        
-            </div></a>
-          <a href='/graphic'>  <div className='rcard1'>
-                <div className='rcard1-top'>
-                <h2>Graphic <br /> Design</h2>
-                <div class="rcard1-btn">
-                      <button><img src={arrow} alt="Description" /></button>
-                    </div>
-        
-                </div>
-                <hr />
-        
-                <div>
-                    <p>
-                       Compelling visuals and marketing materials that reflect your brand identity.
-                    </p>
-                  </div>
-                  <div class="rcard1-image">
-                  <img src="https://demo.awaikenthemes.com/weebix/wp-content/uploads/2024/04/service-img-1.jpg" />
-                </div>
-        
-            </div></a>
-            
-            
-            
-            
-        
+          <div className="related-b1">
+            <a href='/services'>
+              <button>
+                View All related services <img src={arrow} alt="arrow" />
+              </button>
+            </a>
           </div>
-            
-        
         </div>
 
+        <div className='related-card'>
+          {services.map((service, index) => (
+            <a href={service.link} key={index}>
+              <div className='rcard1'>
+                <div className='rcard1-top'>
+                  <h2>{service.title.split(' ')[0]}<br />{service.title.split(' ')[1]}</h2>
+                  <div className="rcard1-btn">
+                    <button><img src={arrow} alt="arrow" /></button>
+                  </div>
+                </div>
+                <hr />
+                <div>
+                  <p>{service.description}</p>
+                </div>
+                <div className="rcard1-image">
+                  <img src={service.image} alt={service.title} />
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
-
-    </>
-  )
+  );
 }
 
-export default Relatedservices
+export default Relatedservices;
--- DDA-Web/src/Pages/Servicespage/UxUiPrice.jsx
+++ DDA-Web/src/Pages/Servicespage/UxUiPrice.jsx
@@ -8,6 +8,26 @@ import Relatedservices from '../../Pages/Servicespage/Relatedservices.jsx';
 import Footer from '../../Pages/Hompage/Footer.jsx';
 
 function UxUiPrice() {
+  const serviceData = [
+    {
+      title: 'Web Development',
+      description: 'Custom, responsive websites that drive performance and visibility.',
+      image: 'https://demo.awaikenthemes.com/weebix/wp-content/uploads/2024/04/service-img-1.jpg',
+      link: '/webdevelopment'
+    },
+    {
+      title: 'App Development',
+      description: 'Smart, user-friendly mobile applications for Android and iOS platforms.',
+      image: 'https://demo.awaikenthemes.com/weebix/wp-content/uploads/2024/04/service-img-1.jpg',
+      link: '/app-price'
+    },
+    {
+      title: 'Graphic Design',
+      description: 'Compelling visuals and marketing materials that reflect your brand identity.',
+      image: 'https://demo.awaikenthemes.com/weebix/wp-content/uploads/2024/04/service-img-1.jpg',
+      link: '/graphic'
+    }
+  ];
   return (
     <>
     <Navbar></Navbar>
@@ -40,7 +60,8 @@ function UxUiPrice() {
               </div>
                                 <PackUxUiPrice></PackUxUiPrice>
 <ChooseUs></ChooseUs>
-<Relatedservices></Relatedservices>
+<Relatedservices services={serviceData} />
+
 <Footer></Footer>
     </>
   )
